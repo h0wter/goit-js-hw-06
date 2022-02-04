d71a9b7d902506392e3a12b122d2d24e176bbd35
h0wter <howter99@gmail.com>
2022-02-04 17:23:44 +0200

    completed task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+const ingr = [];
+ingredients.forEach(item => {
+    const ingredient = document.createElement('li');
+    ingredient.textContent = item;
+    ingredient.classList.add('item');
+    ingredientsList.append(ingredient);
+});
