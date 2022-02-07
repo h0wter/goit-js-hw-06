dba0bc683ccd093227959c39154582ea46b1ed19
h0wter <howter99@gmail.com>
2022-02-07 16:59:56 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+const buttonDecrement = document.querySelector('[data-action="decrement"]');
+const buttonIncrement = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('#value');
+
+//* Option 1
+buttonDecrement.addEventListener('click', () => {
+  value.textContent--;
+});
+buttonIncrement.addEventListener('click', () => {
+  value.textContent++;
+});
+
+//* Option 2
+// let counterValue = 0;
+// buttonDecrement.addEventListener('click', () => {
+//     counterValue--;
+//     value.textContent = counterValue;
+// });
+// buttonIncrement.addEventListener('click', () => {
+//     counterValue++;
+//     value.textContent = counterValue;
+// });
