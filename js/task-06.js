c6f777134d279b56136ab33b558607a8d81fa9a5
h0wter <howter99@gmail.com>
2022-02-07 22:14:06 +0200

    fixed condition

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,13 @@
 const inputRef = document.querySelector('input#validation-input');
 inputRef.addEventListener('blur', () => {
-  if (inputRef.value.length >= inputRef.dataset.length) {
+  if (inputRef.value.trim().length == inputRef.dataset.length) {
     inputRef.classList.remove('invalid');
     inputRef.classList.add('valid');
   }
-  if (inputRef.value.length < inputRef.dataset.length) {
+  if (
+    inputRef.value.trim().length < inputRef.dataset.length ||
+    inputRef.value.trim().length > inputRef.dataset.length
+  ) {
     inputRef.classList.remove('valid');
     inputRef.classList.add('invalid');
   }
