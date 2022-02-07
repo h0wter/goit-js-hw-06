0cbab8ffa69e2c13ba29e739023c4af225ca1e22
h0wter <howter99@gmail.com>
2022-02-07 17:00:15 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('button.change-color');
+const spanRef = document.querySelector('span.color');
+
+btnRef.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  document.body.setAttribute('style', `background-color: ${color}`);
+  spanRef.textContent = color;
+});
