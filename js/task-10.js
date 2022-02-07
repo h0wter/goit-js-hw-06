c2d76d54c90ef7608af62ff386dd209fd01ea34f
h0wter <howter99@gmail.com>
2022-02-07 17:00:22 +0200

    done task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputRef = document.querySelector('#controls > input');
+const boxesRef = document.querySelector('div#boxes');
+const createBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+
+createBtnRef.addEventListener('click', () => {
+  createBoxes(inputRef.value);
+});
+
+destroyBtnRef.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  let string = '';
+  for (let i = 1; i <= amount; i++) {
+    string += createBox(i);
+  }
+  boxesRef.innerHTML = string;
+}
+
+function createBox(ratio) {
+  return `<div style="background-color: ${getRandomHexColor()}; height: ${10 * ratio}px; width: ${
+    10 * ratio
+  }px" ></div>`;
+}
+
+function destroyBoxes() {
+  boxesRef.innerHTML = '';
+}
