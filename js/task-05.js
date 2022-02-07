4558698b501a649e861f98269ea1573311a911b2
h0wter <howter99@gmail.com>
2022-02-07 22:13:45 +0200

    fixed function behavior for string with spaces

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,8 @@ const input = document.querySelector('input#name-input');
 const output = document.querySelector('span#name-output');
 
 input.addEventListener('input', event => {
-  output.textContent = input.value;
-  if (!input.value) {
+  output.textContent = input.value.trim();
+  if (!input.value.trim()) {
     output.textContent = 'Anonymous';
   }
 });
