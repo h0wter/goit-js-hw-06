1f1f10dd5a1148385fe5d848b6ff5ee46dde1c6b
h0wter <howter99@gmail.com>
2022-02-07 17:00:13 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector('form.login-form');
 
+formRef.addEventListener('submit', event => {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === '' || password === '') {
+    return alert('Все поля формы должны быть заполнены!');
+  }
+
+  console.log({
+    mail,
+    password,
+  });
+
+  event.currentTarget.reset();
+});
