e47e80d57a8e1276dfca150a14c1ddac66e17e0d
h0wter <howter99@gmail.com>
2022-02-08 22:17:42 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,9 @@
 const inputRef = document.querySelector('input#validation-input');
 inputRef.addEventListener('blur', () => {
-  if (inputRef.value.trim().length == inputRef.dataset.length) {
+  if (inputRef.value.trim().length === Number(inputRef.dataset.length)) {
     inputRef.classList.remove('invalid');
     inputRef.classList.add('valid');
-  }
-  if (
-    inputRef.value.trim().length < inputRef.dataset.length ||
-    inputRef.value.trim().length > inputRef.dataset.length
-  ) {
+  } else {
     inputRef.classList.remove('valid');
     inputRef.classList.add('invalid');
   }
