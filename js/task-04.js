6edfa605b02f2dcdaa54fee67d4bd31208bc91ca
h0wter <howter99@gmail.com>
2022-02-11 15:12:48 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -3,20 +3,20 @@ const buttonIncrement = document.querySelector('[data-action="increment"]');
 const value = document.querySelector('#value');
 
 //* Option 1
-buttonDecrement.addEventListener('click', () => {
-  value.textContent--;
-});
-buttonIncrement.addEventListener('click', () => {
-  value.textContent++;
-});
-
-//* Option 2
-// let counterValue = 0;
 // buttonDecrement.addEventListener('click', () => {
-//     counterValue--;
-//     value.textContent = counterValue;
+//   value.textContent--;
 // });
 // buttonIncrement.addEventListener('click', () => {
-//     counterValue++;
-//     value.textContent = counterValue;
+//   value.textContent++;
 // });
+
+//* Option 2
+let counterValue = 0;
+buttonDecrement.addEventListener('click', () => {
+  counterValue--;
+  value.textContent = counterValue;
+});
+buttonIncrement.addEventListener('click', () => {
+  counterValue++;
+  value.textContent = counterValue;
+});
